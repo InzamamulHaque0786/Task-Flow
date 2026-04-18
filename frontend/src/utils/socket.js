dc6cd5ec88b3c2bd3c047a8a278d3bb2cb2d232a
InzamamulHaque0786 <imranraza99392582@gmail.com>
2026-04-18 21:23:07 +0530

    frontend cors errors fixed

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -5,7 +5,7 @@ let socket;
 
 export const initiateSocket = (userId) => {
   if (!socket) {
-    socket = io("http://localhost:4000", {
+    socket = io("https://task-flow-ll0a.onrender.com", {
       query: { userId }, 
       withCredentials: true,
       transports: ["websocket", "polling"],
