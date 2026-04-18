92439beda6ce2af9dbd149e7ab196de99bd6c429
InzamamulHaque0786 <imranraza99392582@gmail.com>
2026-04-18 18:35:55 +0530

    last comit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 // Create an instance of axios with the base URL
 const API = axios.create({
   // baseURL:'http://localhost:4000/api',  // Your backend URL
-  baseURL:'https://taskflowbackend-m7a7.onrender.com',  // Your backend URL
+  baseURL:'https://task-flow-ll0a.onrender.com',  // Your backend URL
+  // baseURL:'https://taskflowbackend-m7a7.onrender.com',  // Your backend URL
 });
 
 // Set the JWT token if available
